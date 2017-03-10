828bfaeb1ce719c3710286e0d850fc1859707b94
Rico <richardthomaswhitfield@gmail.com>
2017-03-09 21:42:33 -0600

    user serializer setup

---
+++ api/serializers/user_serializer.py
@@ -0,0 +1,14 @@
+from django.contrib.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'url', 'username', 'email', 'groups')
+
+
+class RestrictedUserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'first_name', 'last_name')
