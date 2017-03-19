48c9749e5b9f653a560fd21468f0d7d23e9274aa
Rico <richardthomaswhitfield@gmail.com>
2017-03-18 20:59:46 -0500

    serializer update

--- api/serializers/tab_serializer.py
+++ api/serializers/tab_serializer.py
@@ -4,9 +4,9 @@ from api.models import tab_model
 
 class TabSerializer(serializers.HyperlinkedModelSerializer):
 	
-	def get_user(self,request):
+	# def get_user(self,request):
 
-		user = serializers.PrimaryKeyRelatedField(many=True, read_only=False, queryset=User.objects.filter(user=request.user), source='user')
+	# 	user = serializers.PrimaryKeyRelatedField(many=True, read_only=False, queryset=User.objects.filter(user=request.user), source='user')
 	class Meta:
 		model = tab_model.Tab
 		fields = (
