15654d40f261e4a386e893d3e4063ca61584fc00
Rico <richardthomaswhitfield@gmail.com>
2017-03-20 11:56:14 -0500

    updates

--- api/serializers/tab_serializer.py
+++ api/serializers/tab_serializer.py
@@ -31,6 +31,6 @@ class TabSerializer(serializers.HyperlinkedModelSerializer):
 	def create(self, validated_data):
 		tab = tab_model.Tab.objects.create(**validated_data)
 		current_user = CurrentUserDefault()
-		tab.user.add(current_user)
+		tab.user.add(current_user.pk)
 
 		return tab
