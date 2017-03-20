33c6989d594b6d80119d1261de2d3f5554367117
Rico <richardthomaswhitfield@gmail.com>
2017-03-20 11:35:52 -0500

    updates

--- api/serializers/tab_serializer.py
+++ api/serializers/tab_serializer.py
@@ -1,18 +1,20 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from api.models import tab_model
+from api.serializers import user_serializer
 from rest_framework.fields import CurrentUserDefault
 
-class TabSerializer(serializers.ModelSerializer):
+class TabSerializer(serializers.HyperlinkedModelSerializer):
 	# user = serializers.PrimaryKeyRelatedField(many=True)
-
-	user = serializers.HiddenField(
-	    default=serializers.CurrentUserDefault()
-	)
+	# user = user_serializer.UserSerializer(many=True)
+	# user = serializers.HiddenField(
+	#     default=serializers.CurrentUserDefault()
+	# )
 
 
 	class Meta:
 		model = tab_model.Tab
+
 		fields = (
 			'user',
 			'artist_url',
@@ -26,3 +28,9 @@ class TabSerializer(serializers.ModelSerializer):
 			'youtube_video_id',
 			'art_url',
 		)
+	def create(self, validated_data):
+		user_data = validated_data.pop('user')
+		tab = tab_model.Tab.objects.create(**validated_data)
+		tab.user.add(self.request.user)
+
+		return tab
