becba3900bd8574106a0970968aff76996ceaf72
Rico <richardthomaswhitfield@gmail.com>
2017-03-21 10:33:35 -0500

    updated settings

--- AstroApi/settings.py
+++ AstroApi/settings.py
@@ -51,13 +51,13 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
 
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ),
+    ],
+    'PAGE_SIZE': 10
 }
 
 MIDDLEWARE = [
