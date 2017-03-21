cfd1a6ade02a8d0bbeb82d191423a37d7ccd6e73
Rico <richardthomaswhitfield@gmail.com>
2017-03-21 10:30:35 -0500

    removed pagination

--- AstroApi/settings.py
+++ AstroApi/settings.py
@@ -55,9 +55,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
 
     ),
-    'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ),
 }
 
 MIDDLEWARE = [
