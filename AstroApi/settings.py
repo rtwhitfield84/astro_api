2190a6f5ab4c8cf6cbcc1e20489cad517d91f1ac
Rico <richardthomaswhitfield@gmail.com>
2017-03-22 20:04:43 -0500

    page lmit

--- AstroApi/settings.py
+++ AstroApi/settings.py
@@ -57,7 +57,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 21
 }
 
 MIDDLEWARE = [
